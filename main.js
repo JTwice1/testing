e497c08d57b4f315f08ee637e49fd674a55e76c3
JTwice1 <trnka.juraj8@gmail.com>
2023-11-16 18:08:11 +0100

    AI repair

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
+// const music = new Audio('./assets/sounds/monitorBeep.mp3');
+
+// function stopMusic() {
+//  music.currentTime = 0;
+//  music.pause();
+// }
+
+// function restartMusic() {
+//  music.currentTime = 0;
+//  music.play();
+//  music.volume = 0.7;
+// }
+// music.addEventListener('ended', function () {
+//  restartMusic();
+// });
+
+// restartMusic();
+
+// console.log('test');
+
 const music = new Audio('./assets/sounds/monitorBeep.mp3');
+let musicStarted = false;
 
 function stopMusic() {
  music.currentTime = 0;
@@ -7,9 +28,24 @@ function stopMusic() {
 
 function restartMusic() {
  music.currentTime = 0;
- music.play();
- music.volume = 0.7;
+ music.play().then(() => {
+  // Autoplay started successfully
+  music.volume = 0.7;
+  musicStarted = true;
+ }).catch(error => {
+  // Autoplay was prevented
+  console.log('Autoplay was prevented:', error);
+  // In case autoplay was prevented, set up an event listener for user interaction
+  document.addEventListener('click', startMusicOnInteraction);
+ });
 }
+
+function startMusicOnInteraction() {
+ if (!musicStarted) {
+  restartMusic();
+ }
+}
+
 music.addEventListener('ended', function () {
  restartMusic();
 });
